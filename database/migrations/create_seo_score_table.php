549692a222412526a09023f707345dda24572827
Robin van Maasakker <robin@qubiqx.com>
2024-04-08 15:38:03 +0200

    Update

--- database/migrations/create_seo_score_table.php
+++ database/migrations/create_seo_score_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,7 +14,8 @@ return new class extends Migration
     {
         Schema::create('seo_scores', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('seo_scan_id');
+            $table->unsignedBigInteger('seo_scan_id')
+                ->nullable();
             $table->string('url');
             $table->nullableMorphs('model', 'model');
             $table->integer('score');
